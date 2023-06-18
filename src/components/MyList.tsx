363e6f6b411cd003e715b9dc710f196b3ab2f876
Mauricio Bernardes <mauriciosoria91@gmail.com>
2023-06-18 01:24:36 -0300

    atualização de versao do rn

--- src/components/MyList.tsx
+++ src/components/MyList.tsx
@@ -1,19 +1,105 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text,View,ScrollView,TouchableOpacity,Image} from 'react-native';
 
 class MyList extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text>Hello, world!</Text>
-      </View>
-    );
-  }
+  
+    state = {
+        loading: false,
+        data: [],
+        current_page: 1,
+        error: null,
+        hasMore: true
+    }
+
+    constructor(props) {
+        super(props);
+        this.state.data=this.createData();
+        
+    }
+
+    createData = () => {
+        
+        let newData = [];
+        newData.push({
+            title: "Lorem ipsum", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in congue risus, non viverra tellus. Nam faucibus ligula non metus ultrices mollis. Cras dolor purus, hendrerit eu eros quis, dignissim eleifend mi. In tincidunt mi in diam egestas congue ac ut purus. Nulla semper libero vitae blandit vehicula.",
+            image: require('../images/img1.png'),
+            id: this.state.data.length
+        });
+        newData.push({
+            title: "Curabitur vulputate", 
+            text: "Curabitur vulputate enim in lacus imperdiet, a convallis odio posuere. Nulla id ex et purus sodales rutrum non eu eros. Ut consequat est lacus.",
+            image: require('../images/img2.png'),
+            id: this.state.data.length+1
+        });
+        newData.push({
+            title: "Proin hendrerit", 
+            text: "Proin hendrerit nisl id turpis bibendum, sit amet scelerisque augue elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a blandit sapien.",
+            image: require('../images/img3.png'),
+            id: this.state.data.length+2
+        });
+        return newData;
+       
+    }
+
+    getListOfData = () => {
+        //if (this.state.loading) { return; }
+       // this.setState({ loading: true });
+        let newData = [];
+        newData.push({
+            title: "Lorem ipsum", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in congue risus, non viverra tellus. Nam faucibus ligula non metus ultrices mollis. Cras dolor purus, hendrerit eu eros quis, dignissim eleifend mi. In tincidunt mi in diam egestas congue ac ut purus. Nulla semper libero vitae blandit vehicula.",
+            image: "img1.png",
+            id: this.state.data.length
+        });
+        newData.push({
+            title: "Curabitur vulputate", 
+            text: "Curabitur vulputate enim in lacus imperdiet, a convallis odio posuere. Nulla id ex et purus sodales rutrum non eu eros. Ut consequat est lacus.",
+            image: "img2.png",
+            id: this.state.data.length+1
+        });
+        newData.push({
+            title: "Proin hendrerit", 
+            text: "Proin hendrerit nisl id turpis bibendum, sit amet scelerisque augue elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a blandit sapien.",
+            image: "img3.png",
+            id: this.state.data.length+2
+        });
+        this.setState({
+            hasMore: true,        
+            data: [...this.state.data, ...newData],
+            loading: false,
+            current_page: this.state.current_page + 1
+        });
+       
+    }
+
+    handleScroll(nativeEvent) {
+
+        console.log(nativeEvent)
+    }    
+
+    renderList = () => {
+        return ( this.state.data.map((u) => {
+            return ( 
+            <TouchableOpacity key={u.id}>
+                <View style={{ padding: 10 }}>
+                
+                    <Image source={u.image} />
+                    <Text style={{ fontSize: 15}}>{u.title}</Text> 
+                    <Text>{u.text}</Text>
+                </View>
+             </TouchableOpacity>);
+            })
+       );
+    }
+  
+    render() {
+        return (
+            <ScrollView onScroll={this.handleScroll}> 
+            {this.renderList()} 
+          </ScrollView>
+          );
+    }
 }
 
 export default MyList;
